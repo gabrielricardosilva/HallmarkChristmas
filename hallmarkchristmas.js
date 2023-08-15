dd620cf040b0cdd57ec3dbd928f3485ae7e1e6b7
Gabriel Ricardo <93540978+gabrielricardosilva@users.noreply.github.com>
2023-08-15 15:23:18 -0300

    Hallmark Chrismtas

---
+++ hallmarkchristmas.js
@@ -0,0 +1,9 @@
+let cakeJson = [
+    {id:1, name:'Um Misterio de Natal', img:'images/UmMisterioDeNatalPoster.jpg', price:0, sizes:['480p', '720p', '1080p'], description:'Uma fita dos sinos do Papai Noel desaparece poucos dias antes do Natal. Um grupo de crianças intrépidas tenta encontrar o verdadeiro ladrão e restaurar a magia natalina de sua cidadezinha.'},
+    {id:2, name:'Era uma Vez em Roma', img:'images/RomeInLovePoster.jpg', price:0, sizes:['480p', '720p', '1080p'], description:'Descrição do seu bolo com várias linhas muito legal e interessante'},
+    {id:3, name:'Summer Dreams', img:'images/SummerDreamsPoster.jpg', price:0, sizes:['480p', '720p', '1080p'], description:'Sinopse do Filme'},
+    {id:4, name:'Summer Love', img:'images/SummerLovePoster.jpg', price:0, sizes:['480p', '720p', '1080p'], description:'Sinopse do Filme'},
+    {id:5, name:'A Summer Romance', img:'images/SummerRomancePoster.png', price:0, sizes:['480p', '720p', '1080p'], description:'Sinopse do Filme'},
+    {id:6, name:'Beach House', img:'images/BeachHousePoster.png', price:0, sizes:['480p', '720p', '1080p'], description:'Sinopse do Filme'},
+    {id:7, name:'Nome do Filme', img:'images/bolo7.png', price:0, sizes:['480p', '720p', '1080p'], description:'Sinopse do Filme'}
+];
